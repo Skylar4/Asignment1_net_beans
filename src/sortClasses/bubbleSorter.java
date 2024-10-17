c82fe36d72692a857cc50be624ef96f1662240ea
Hmmm8938 <39926063+Hmmm-8938@users.noreply.github.com>
2024-10-16 18:02:10 -0600

    Updating + Adding Bubble Sort Current Progress

---
+++ src/sortClasses/bubbleSorter.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sortClasses;
+import shapes.Shape;
+
+/**
+ *
+ * @author benjaminmellott
+ */
+
+public class bubbleSorter 
+{
+    public static Shape[] bubbleSort(Shape[] shapesList)
+    {
+        for (Shape i : shapesList)
+        {
+            Shape[] temp = new Shape[2];
+            if (i[i].getHeight().compareTo(temp[0]) == 1)
+            {
+                
+            }
+        }
+        return shapesList;
+    }
+    
+}
