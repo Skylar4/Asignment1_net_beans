468f9c68c2eaafd673bf74d8db7b7cfd047c4b98
Marian <39353287+ian-ann@users.noreply.github.com>
2024-10-26 21:07:23 -0600

    Marian (#18)
    * Update on Shape Class
    
    changed empty methods to abstract, moved ShapeComparator to utilities package
    
    * Update readMe.txt
    
    * updated shapes and sorting order
    
    removed the ToString methods in the the sub-class of shapes as this is conflicting with the result output.
    updated the sorting order for merge and quick sort.
    Added some system output for debugging
    Changed for loop to switch
    removed hardcode value for the interval in result output
    
    * Delete Sort.jar
    
    * renamed classes to TitleCase
    
    * fixed conflict, updated class name for consistency
    
    Sort.jar is from the build that creates a copy of the jar from dist
    
    * Squashed commit of the following:
    
    commit 9aa0f0bede1c8a482685bbac0f35649e6f22f872
    Author: Skylar4 <95008499+Skylar4@users.noreply.github.com>
    Date:   Thu Oct 24 10:18:40 2024 -0600
    
        fixed selection sorter
    
    commit efabc0f62109f0152884663666005f9bcd9e0648
    Author: Marian <39353287+ian-ann@users.noreply.github.com>
    Date:   Wed Oct 23 19:31:01 2024 -0600
    
        Marian (#15)
    
        * Update on Shape Class
    
        changed empty methods to abstract, moved ShapeComparator to utilities package
    
        * Update readMe.txt
    
        * updated shapes and sorting order
    
        removed the ToString methods in the the sub-class of shapes as this is conflicting with the result output.
        updated the sorting order for merge and quick sort.
        Added some system output for debugging
        Changed for loop to switch
        removed hardcode value for the interval in result output
    
        * Delete Sort.jar
    
        * renamed classes to TitleCase
    
        * fixed conflict, updated class name for consistency
    
        Sort.jar is from the build that creates a copy of the jar from dist
    
    * minor updates on filenames
    
    * updated filenames
    
    * minor changes for consistency
    
    * Refactored code for easy reading

--- src/utilities/MergeSorter.java
+++ src/utilities/MergeSorter.java
@@ -7,11 +7,11 @@ import java.util.Arrays;
 /**
  * This class sorts the Polygons based on the compare type.
  * 
- * @author Marian Estrada
+ * @param <T> extends Shape Class
  */
 public class MergeSorter<T extends Shape> {
-    private String compareType;
-    private T[] array;
+    private final String compareType;
+    private final T[] array;
 
     /**
      * @param compareType the type of comparison to be used
@@ -23,10 +23,13 @@ public class MergeSorter<T extends Shape> {
     }
 
     /**
-     * @param left
-     * @param right
+     * Sorts the array using merge sort algorithm.
      */
-    public void mergeSort(int left, int right) {
+    public void mergeSort() {
+        mergeSort(0, array.length - 1);
+    }
+
+    private void mergeSort(int left, int right) {
         if (left < right) {
             int mid = (left + right) / 2;
             mergeSort(left, mid);
@@ -45,37 +48,31 @@ public class MergeSorter<T extends Shape> {
         int i = 0, j = 0, k = left;
 
         while (i < n1 && j < n2) {
-            int comparisonResult = compareBy(leftArray[i], rightArray[j], compareType);
-            if (comparisonResult >= 0) { // Change the comparison sign for descending order
-                array[k] = leftArray[i];
-                i++;
+            if (compareBy(leftArray[i], rightArray[j], compareType) >= 0) {
+                array[k++] = leftArray[i++];
             } else {
-                array[k] = rightArray[j];
-                j++;
+                array[k++] = rightArray[j++];
             }
-            k++;
         }
 
         while (i < n1) {
-            array[k] = leftArray[i];
-            i++;
-            k++;
+            array[k++] = leftArray[i++];
         }
 
         while (j < n2) {
-            array[k] = rightArray[j];
-            j++;
-            k++;
+            array[k++] = rightArray[j++];
         }
     }
 
     /**
-     * @param <T>
-     * @param compareType
-     * @param array
+     * Static method to sort an array of shapes using merge sort.
+     * 
+     * @param <T> extends Shape
+     * @param compareType the type of comparison to be used
+     * @param array the array of shapes to be sorted
      */
     public static <T extends Shape> void sort(String compareType, T[] array) {
         MergeSorter<T> sorter = new MergeSorter<>(compareType, array);
-        sorter.mergeSort(0, array.length - 1);
+        sorter.mergeSort();
     }
 }
