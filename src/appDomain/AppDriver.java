9790c86f3a4bdcd99f7a41995ffed2bfee145158
marian.estrada <ian_an@yahoo.com>
2024-10-28 17:00:22 -0600

    Removed arguments default value that was used during testing

--- src/appDomain/AppDriver.java
+++ src/appDomain/AppDriver.java
@@ -10,24 +10,17 @@ import utilities.InsertionSorter;
 import utilities.QuickSorter;
 import utilities.RadixSorter;
 
-public class AppDriver 
-{
+public class AppDriver {
 
     private static final String FILE_PREFIX = "-f";
     private static final String COMPARE_PREFIX = "-t";
     private static final String SORT_PREFIX = "-s";
     private static final int INTERVAL = 1000;
 
-    public static void main(String[] args) 
-    {
-        String fileName = null;
-        String compareType = null;
-        String sortType = null;
-
-        // Comment out to use command line; for testing only
-//        fileName = "shapes2.txt";
-//        compareType = "a";
-//        sortType = "selection";
+    public static void main(String[] args) {
+        String fileName;
+        String compareType;
+        String sortType;        
 
         String[] parsedArgs = parseArguments(args);
         fileName = parsedArgs[0];
@@ -36,8 +29,7 @@ public class AppDriver
 
         System.out.println("Filename " + fileName + "; Compare By " + compareType + "; Sort Type: " + sortType);
 
-        if (fileName == null || compareType == null || sortType == null) 
-        {
+        if (fileName == null || compareType == null || sortType == null) {
             System.out.println("Usage: java -jar Sort.jar -f<file_name> -t<compare_type> -s<sort_type>");
             return;
         }
@@ -54,87 +46,64 @@ public class AppDriver
         System.out.println("run time was: " + (endTime - startTime) + " milliseconds");
     }
 
-    private static String[] parseArguments(String[] args) 
-    {
-        String fileName = "shapes4.txt";
-        String compareType = "a"; // Valid types for this are (h (height), v (volume), a (area))
-        String sortType = "i";
+    private static String[] parseArguments(String[] args) {
+        String fileName = null;
+        String compareType = null;
+        String sortType = null;
 
-        for (String arg : args) 
-        {
-            if (arg.startsWith(FILE_PREFIX)) 
-            {
+        for (String arg : args) {
+            if (arg.toLowerCase().startsWith(FILE_PREFIX)) {
                 fileName = arg.substring(FILE_PREFIX.length());
-            } 
-            else if (arg.startsWith(COMPARE_PREFIX)) 
-            {
+            } else if (arg.toLowerCase().startsWith(COMPARE_PREFIX)) {
                 compareType = arg.substring(COMPARE_PREFIX.length()).toLowerCase();
-            } 
-            else if (arg.startsWith(SORT_PREFIX)) 
-            {
+            } else if (arg.toLowerCase().startsWith(SORT_PREFIX)) {
                 sortType = arg.substring(SORT_PREFIX.length()).toLowerCase();
             }
         }
         return new String[]{fileName, compareType, sortType};
     }
 
-    private static void sortShapes(String sortType, String compareType, Shape[] shapes) 
-    {
-        switch (sortType) 
-        {
-            case "m" -> 
-            {
+    private static void sortShapes(String sortType, String compareType, Shape[] shapes) {
+        switch (sortType) {
+            case "m" -> {
                 System.out.println("Sorting by Merge Sort algorithm");
                 MergeSorter.sort(compareType, shapes);
             }
-            case "b" -> 
-            {
+            case "b" -> {
                 System.out.println("Sorting by Bubble Sort algorithm");
                 BubbleSorter.sort(compareType, shapes);
             }
-            case "s" -> 
-            {
+            case "s" -> {
                 System.out.println("Sorting by Selection Sort algorithm");
                 SelectionSorter.Sort(shapes, compareType);
             }
-            case "i" -> 
-            {
+            case "i" -> {
                 System.out.println("Sorting by Insertion Sort algorithm");
                 InsertionSorter.sort(shapes, compareType);
             }
-            case "q" -> 
-            {
+            case "q" -> {
                 System.out.println("Sorting by Quick Sort algorithm");
                 QuickSorter.sort(compareType, shapes);
             }
-            case "r" -> 
-            {
+            case "r" -> {
                 System.out.println("Sorting by Radix Sort algorithm");
                 RadixSorter.sort(compareType, shapes);
             }
-            default -> throw new IllegalArgumentException("Invalid sort type: " + sortType);
+            default ->
+                throw new IllegalArgumentException("Invalid sort type: " + sortType);
         }
     }
 
-    private static void printShapes(Shape[] shapes, String compareType) 
-    {
-        for (int i = 0; i < shapes.length; i++) 
-        {
+    private static void printShapes(Shape[] shapes, String compareType) {
+        for (int i = 0; i < shapes.length; i++) {
             Shape shape = shapes[i];
-            if (i == 0) 
-            {
+            if (i == 0) {
                 System.out.println("First Element is: " + shape.toString(compareType));
-            } 
-            else if (i == shapes.length - 2) 
-            {
+            } else if (i == shapes.length - 2) {
                 System.out.println("Second Last element is: " + shape.toString(compareType));
-            } 
-            else if (i == shapes.length - 1) 
-            {
+            } else if (i == shapes.length - 1) {
                 System.out.println("Last element is: " + shape.toString(compareType));
-            } 
-            else if (i % INTERVAL == 0) 
-            {
+            } else if (i % INTERVAL == 0) {
                 System.out.println(i + "-th element is: " + shape.toString(compareType));
             }
         }
